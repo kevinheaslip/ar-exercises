da59998069b078d88c8e5a70d2dcbe5ecd801c89
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-10-11 12:54:47 -0700

    Increase hourly_rates to pass validation

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,9 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@store1.employees.create(first_name: "Bill", last_name: "Gates", hourly_rate: 5)
-@store1.employees.create(first_name: "Steve", last_name: "Jobs", hourly_rate: 7)
+@store1.employees.create(first_name: "Bill", last_name: "Gates", hourly_rate: 40)
+@store1.employees.create(first_name: "Steve", last_name: "Jobs", hourly_rate: 50)
 @store1.employees.create(first_name: "Steve", last_name: "Wozniak", hourly_rate: 100)
-@store2.employees.create(first_name: "George", last_name: "Bush", hourly_rate: 1)
-store2.employees.create(first_name: "Bernie", last_name: "Sanders", hourly_rate: 20)
-store2.employees.create(first_name: "Barack", last_name: "Obama", hourly_rate: 3)
+@store2.employees.create(first_name: "George", last_name: "Bush", hourly_rate: 60)
+store2.employees.create(first_name: "Bernie", last_name: "Sanders", hourly_rate: 70)
+store2.employees.create(first_name: "Barack", last_name: "Obama", hourly_rate: 80)
